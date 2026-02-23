6810772ce4935a84b90e7bb3b4bc544a9513a879
aasthaBaid <aasthabaid3@gmail.com>
2026-02-23 14:33:23 +0530

    adding uc1, initialise the game rules

---
+++ GuessApp.java
@@ -0,0 +1,13 @@
+/*
+Initialize 
+@author developer
+@version 1.0
+*/
+import java.util.*;
+public class GuessApp {
+	public static void main(String[] args){
+		System.out.println("Welcome to the game");
+		GameConfig game = new GameConfig();
+		game.showRules();
+	}	
+}
